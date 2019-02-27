fa26b0b711956179bba3de0dce89125e93c2ded5
PgeorgLim <panayiotis_geo@hotmail.com>
2019-02-27 10:26:05 +0200

    Updates

---
+++ precache-manifest.245d0efc2149769b3272d28771b50d83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "b1dca7eb1e611dfd47d8",
+    "url": "/memory-game/static/js/main.b1dca7eb.chunk.js"
+  },
+  {
+    "revision": "eaf91d9c8b6b72c57c51",
+    "url": "/memory-game/static/js/1.eaf91d9c.chunk.js"
+  },
+  {
+    "revision": "b1dca7eb1e611dfd47d8",
+    "url": "/memory-game/static/css/main.2c7a9b28.chunk.css"
+  },
+  {
+    "revision": "ab16b44657b6826643aeeee523d4df18",
+    "url": "/memory-game/index.html"
+  }
+];
